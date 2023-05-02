13ed78c7a44ee6a79d2c2a955dfaa72ad7ec2e86
NS-Sheam <123sheamfeni@gmail.com>
2023-05-02 12:27:53 +0600

    adding footer section

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './pages/shared/Footer/Footer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Footer/>
   </React.StrictMode>,
 )
