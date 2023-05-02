54092ae0098da5538877cec2085e939bdd1cf603
NS-Sheam <123sheamfeni@gmail.com>
2023-05-02 13:03:57 +0600

    adding footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  RouterProvider,
+} from "react-router-dom";
 import Footer from './pages/shared/Footer/Footer.jsx';
+import { router } from './routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Footer/>
+    <RouterProvider router={router} ></RouterProvider>
   </React.StrictMode>,
 )
--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -1,64 +1,58 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="bg-secondary text-white text-center">
-                <div className="container p-4">
-                    <div className="row">
-                        <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                            <h5 className="text-uppercase">Footer Content</h5>
-
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                                molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-                                aliquam voluptatem veniam, est atque cumque eum delectus sint!
-                            </p>
-                        </div>
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 className="text-uppercase">Links</h5>
-
-                            <ul className="list-unstyled mb-0">
-                                <li>
-                                    <a href="#!" className="text-white">Link 1</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white">Link 2</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white">Link 4</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 className="text-uppercase mb-0">Links</h5>
-
-                            <ul className="list-unstyled">
-                                <li>
-                                    <a href="#!" className="text-white">Link 1</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white">Link 2</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white">Link 3</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white">Link 4</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-                    © 2020 Copyright:
-                    <a className="text-white" href="/">MDBootstrap.com</a>
-                </div>
-            </footer>
-        </div>
+        <footer className="bg-secondary text-white text-center">
+            <Container className="p-4">
+                <Row>
+                    <Col lg={6} md={12} className="mb-4 mb-md-0">
+                        <h5 className="text-uppercase">Footer Content</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam, est atque cumque eum delectus sint!
+                        </p>
+                    </Col>
+                    <Col lg={3} md={6} className="mb-4 mb-md-0">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled mb-0">
+                            <li>
+                                <Link to="#!" className="text-white">Link 1</Link>
+                            </li>
+                            <li>
+                                <Link to="#!" className="text-white">Link 2</Link>
+                            </li>
+                            <li>
+                                <Link to="#!" className="text-white">Link 3</Link>
+                            </li>
+                            <li>
+                                <Link to="#!" className="text-white">Link 4</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col lg={3} md={6} className="mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-0">Links</h5>
+                        <ul className="list-unstyled">
+                            <li>
+                                <Link to="#!" className="text-white">Link 1</Link>
+                            </li>
+                            <li>
+                                <Link to="#!" className="text-white">Link 2</Link>
+                            </li>
+                            <li>
+                                <Link to="#!" className="text-white">Link 3</Link>
+                            </li>
+                            <li>
+                                <Link to="#!" className="text-white">Link 4</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+            <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                © {new Date().getFullYear()} My Company
+            </div>
+        </footer>
     );
 };
 
