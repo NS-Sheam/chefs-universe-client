778a7e2716751497c9e2de1f0e76e88d98f28f58
NS-Sheam <123sheamfeni@gmail.com>
2023-05-02 23:43:30 +0600

    recipi card almost done

--- src/pages/chefRecipies/Recipies/Recipies.jsx
+++ src/pages/chefRecipies/Recipies/Recipies.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import SingleRecipies from '../SingleRecipies/SingleRecipies';
 
-const Recipies = ({recipeData}) => {
+const Recipies = ({ recipeData }) => {
+    // console.log(recipeData);
     return (
         <div className='my-5'>
             <h1 className='mb-5'>My Recipies</h1>
             <Row className='mt-4 mb-4'>
-            {
-                recipeData.map(data => <Col xs={12} md={6} lg={4} className=''>
-                    <SingleRecipies
-                        key={data.recipeId}
-                        data={data} />
-                </Col>)
-            }
-        </Row>
+                {
+                    recipeData.map(data => <Col xs={12} md={6} lg={4} className=''>
+                        <SingleRecipies
+                            key={data.recipeId}
+                            data={data} />
+                    </Col>)
+                }
+            </Row>
         </div>
     );
 };
--- src/pages/chefRecipies/SingleRecipies/SingleRecipies.jsx
+++ src/pages/chefRecipies/SingleRecipies/SingleRecipies.jsx
@@ -1,11 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { FaCheck } from 'react-icons/fa';
+import "./SingleRecipies.css"
 
-const SingleRecipies = ({ recipeData }) => {
-    const { recipeName, recipeImg, ingredients, methodOfCooking, rating } = recipeData;
+const SingleRecipies = ({ data }) => {
+    const { recipeName, recipeImg, ingredients, methodOfCooking, rating } = data;
+    // console.log(data);
+    // console.log(ingredients);
     return (
-        <div>
-
-        </div>
+        <Card className='chefCard mb-4 h-100'>
+            <div className='h-25'>
+                <Card.Img className='h-100' variant="top" src={recipeImg} />
+            </div>
+            <Card.Body>
+                <Card.Title className='band-color fw-bold h3'>{recipeName}</Card.Title>
+                <Card.Text className='text-start'> <span className='fw-bold'>Ingredients: </span>
+                    {
+                        ingredients.map((ing, i) => <p className=''>
+                            <FaCheck /> {ing}
+                        </p>)
+                    }
+                    <p className='text-start'>
+                        <span className='fw-bold text-decoration-underline'>Cooking Process:</span> {methodOfCooking}
+                    </p>
+                    <p className='fw-bold d-flex justify-content-between'>
+                        <span>Rating: {rating}</span>
+                        <span>Fav</span>
+                    </p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 };
 
