50ff107633ffed2651d3b75435dfe92e5caec29c
NS-Sheam <123sheamfeni@gmail.com>
2023-05-02 15:03:29 +0600

    banner page almost done

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import chefPng1 from "../../../assets/chefpng1.png"
+import chefPng2 from "../../../assets/chefpng2.png"
+import chefPng3 from "../../../assets/chefpng3.png"
+
+const Banner = () => {
+    return (
+        <Carousel>
+            <Carousel.Item interval={500}>
+
+                <div className='my-5 d-flex flex-column flex-md-row align-items-center justify-content-center vh-lg-100'>
+                    <img className='w-50' src={chefPng1} alt="" />
+                    <div>
+                        <h1 className='logo fw-bold band-color text-lg text-end'>Welcome to the universe of <br /> <span>dalicious recipies</span></h1>
+                        <p className='band-secondary text-sm-end me-0'>Discover a world of delicious and easy-to-follow recipes for any occasion. From classic family meals to creative and exotic dishes, our collection has something for everyone. With step-by-step instructions and beautiful photos, cooking has never been more enjoyable. Start exploring now and let's cook up something amazing together!</p>
+                    </div>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+                <div className="my-5 d-flex flex-column flex-md-row align-items-center justify-content-center vh-lg-100">
+                    <div>
+                        <h1 className='logo fw-bold band-color text-lg text-start'>Welcome to the universe of <br /> <span>dalicious recipies</span></h1>
+                        <p className='band-secondary text-sm-start'>Discover a world of delicious and easy-to-follow recipes for any occasion. From classic family meals to creative and exotic dishes, our collection has something for everyone. With step-by-step instructions and beautiful photos, cooking has never been more enjoyable. Start exploring now and let's cook up something amazing together!</p>
+                    </div>
+                    <img className='w-50' src={chefPng2} alt="" />
+                </div>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+                <div className="my-5 d-flex flex-column flex-md-row align-items-center justify-content-center vh-lg-100">
+                    <img className='w-50' src={chefPng3} alt="" />
+                    <div>
+                        <h1 className='logo fw-bold band-color text-lg text-end'>Welcome to the universe of <br /> <span>dalicious recipies</span></h1>
+                        <p className='band-secondary text-sm-end me-0'>Discover a world of delicious and easy-to-follow recipes for any occasion. From classic family meals to creative and exotic dishes, our collection has something for everyone. With step-by-step instructions and beautiful photos, cooking has never been more enjoyable. Start exploring now and let's cook up something amazing together!</p>
+                    </div>
+                </div>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Banner;
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,16 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FaAccessibleIcon, FaUser } from "react-icons/fa";
 import "./Header.css"
 
 function Header() {
     return (
         <Navbar className='header' expand="lg">
             <Container>
-                <Navbar.Brand href="#home" className='text-white'>React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="#home" className='text-white'><small className='logo fw-bold'>React-Bootstrap</small></Navbar.Brand>
                 <div>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link className='text-white' to="#home">Home</Nav.Link>
@@ -18,13 +18,15 @@ function Header() {
                             <Nav.Link className='text-white' to="#link">About</Nav.Link>
                             <Nav.Link className='text-white' to="#link">Contact</Nav.Link>
                         </Nav>
-                    </Navbar.Collapse>
-                </div>
-                <div>
-                <Nav className="me-auto">
-                            <Nav.Link className='text-white' to="#home">Home</Nav.Link>
-                            <Nav.Link className='text-white' to="#link">Link</Nav.Link>
+                        <Nav className="me-auto align-items-center flex-grow-1">
+                            <Nav.Link className='text-white' to="#home">
+                                <small className='border border-2 border-white rounded-circle p-1 d-flex justify-content-center align-items-center'>
+                                    <FaUser />
+                                </small>
+                            </Nav.Link>
+                            <Nav.Link className='text-white fw-bold' to="#link">Logout</Nav.Link>
                         </Nav>
+                    </Navbar.Collapse>
                 </div>
             </Container>
         </Navbar>
